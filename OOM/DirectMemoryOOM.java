6d6f8e6d87f35a933cf63776ab559d0bb4c628a1
binshion <binshion@hotmail.com>
2019-09-02 19:04:36 +0800

    DirectMemoryOOM

--- OOM/DirectMemoryOOM.java
+++ OOM/DirectMemoryOOM.java
@@ -4,8 +4,6 @@ import java.lang.reflect.Field;
 
 /**
  * VM Args:-Xmx20M -XX:MaxDirectMemorySize=10M
- *
- * 执行卡机。。谨慎操作
  */
 public class DirectMemoryOOM {
     public static void main(String[] args) throws Exception {
@@ -14,7 +12,7 @@ public class DirectMemoryOOM {
         Unsafe unsafe = (Unsafe) unsafeField.get(null);
 
         while (true) {
-            unsafe.allocateMemory(1024L);
+            unsafe.allocateMemory(1024 * 1024);
         }
     }
 }
